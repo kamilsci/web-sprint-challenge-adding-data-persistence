6053e88cdbc884c5b270c49a8a326b45485dec66
kamilsci <sunnysideprd@gmail.com>
2025-01-19 18:12:49 -0800

    done

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,36 @@
 // build your `Project` model here
+// `project_id` - primary key
+//   - [ ] `project_name` - required
+//   - [ ] `project_description` - optional
+//   - [ ] `project_completed` - the database defaults it to `false` (integer 0) if not provided
+const db = require('../../data/dbConfig');
+module.exports = {
+    get,
+    insert,
+}
+function get() {
+    return db('projects')
+    .then(projects => {
+        return projects.map(project => {
+            return {
+                ...project,
+                project_completed: project.project_completed ? true : false
+            }
+        })
+    })
+}
+function insert(project) {
+    return db('projects')
+    .insert(project)
+    .then(([project_id]) => {
+        return db('projects')
+        .where('project_id', project_id)
+        .first()
+        .then(project => {
+            return {
+                ...project,
+                project_completed: project.project_completed ? true : false
+            }
+        })
+    })
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,20 @@
+ 
 // build your `/api/projects` router here
+const express = require('express');
+const router = express.Router();
+const Projects = require('./model');
+router.get('/', (req, res, next) => {
+    Projects.get()
+    .then(projects => {
+        res.json(projects)
+    })
+    .catch(next)
+})
+router.post('/', (req, res, next) => {
+    Projects.insert(req.body)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(next)
+})
+module.exports = router;
